f0a7dd3ad6b9989f0f629ee0e5942135c9f4be17
jaypranav <jayanthi.g@gmail.com>
2015-09-22 13:50:02 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -54,78 +54,32 @@ function readytologin(e)
 	e.preventDefault();
 };
 
-function register11(e)
-{
-	alert("FIRST NAME:  + fname"); 
-	//var fname = document.getElementById('fname').textContent;
-		 
-};
-
 jQuery( document ).ready( function($) {
 	
 	$("#register-btn").click(function() {
 		 var datastring = $("#register").serialize();
 		var register_url = "http://imajineweb.com/temple/insert_user.php";
-		 // var register_url = "insert_user.php";
 		 alert("first");
 		 $.ajax({
-			// url: 'http://www.imajineweb.com/temple/insert_user.php',
-			 url: register_url,
+			url: register_url,
 			 data: datastring, // Convert a form to a JSON string representation
 			 type: 'post', 
 			 async: true,
 			 
 			 beforeSend: function() {
-				 alert("SECOND");
-				 // This callback function will trigger before data is sent
-				// $.mobile.showPageLoadingMsg(true); // This will show ajax spinner
-				 },
-				 complete: function() {
-					 alert("FINAL");
-					 // This callback function will trigger on data sent/received complete
-				//	 $.mobile.hidePageLoadingMsg(); // This will hide ajax spinner
-					 },
+				 alert("IN THE PROCESS OF ADDING YOUR RECORD");
+				},
+				 complete: function() {  alert("DONE!"); },
 					 success: function (result) {
-						 alert("FDONE");
-                           /* resultObject.formSubmitionResult = result;
-                                        $.mobile.changePage("#second"); */
+						 alert(result);
+                           
                     },
-                    error: function (request,error) {
-                        // This callback function will trigger on unsuccessful action                
-                        alert('Network error has occurred please try again!');
-                    }
 					
                 });   
 				 alert(datastring);                
                
-            return false; // cancel original event to prevent form submitting
-		/* 
-		 $.ajax({
-			type       : "POST",
-			url        : register_url,
-			crossDomain: true,
-			beforeSend : function() {$.mobile.loading('show')},
-			complete   : function() {$.mobile.loading('hide')},
-			// data       : {username : 'subin', password : 'passwordx'},
-			 data       : datastring,
-			dataType   : 'json',
-			success    : function(response) {
-				//console.error(JSON.stringify(response));
-				alert('Works!');
-				},
-    		error      : function() {
-				
-       		// console.error("error");
-			alert('Now working!');                  
-    }
-});  */
+            return false; 
 		
 		
 	});
-		
-	
-	
-	
-	
-	
 	});
